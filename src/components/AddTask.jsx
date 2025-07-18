8d090dcc52847678cfd78e7eb4a1b40140f01759
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-07-18 23:08:56 +0530

    Done

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,11 +1,10 @@
 
-
 const AddTask = (props) => {
 
 
  
   return (
-    <div className="lft h-[100%] w-[50%] bg-black flex items-center flex-col">
+    <div className="lft h-[100%] w-[50%] bg-black flex items-center flex-col add-task">
       <h1 className='text-[clamp(1.5rem,3vw,5rem)] font-bold text-white my-10'>
         Set <span className='font-black bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'>Tasks</span>
       </h1>
@@ -26,7 +25,7 @@ onClick={() => {
 }}
 
          
-          className='my-10 hover:bg-red-900 active:scale-90 rounded-sm font-bold p-1.5 cursor-pointer font-black bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent'
+          className='my-10 hover:bg-red-900 active:scale-90 rounded-sm font-bold p-1.5 cursor-pointer btn'
         >
           Create Task
         </button>
