d7e475fcbac380430341b55aaf3f9a925378bae8
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-07-18 23:12:49 +0530

    Done

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -5,7 +5,7 @@ import { Typewriter } from 'react-simple-typewriter';
 const Head = () => {
   return (
     <div className='flex justify-center items-center gap-2 bg-black h-[10%]'>
-      <h1 className='text-[clamp(1.5rem,3.5vw,5rem)] font-black bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent my-20'>
+      <h1 className='text-[clamp(2.3rem,3.5vw,5rem)] font-black bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 bg-clip-text text-transparent my-20'>
         <Typewriter
           words={['Task Tracker ']}
           loop={0}
@@ -16,7 +16,7 @@ const Head = () => {
           delaySpeed={1000}
         />
       </h1>
-      <FaNotesMedical className='text-[clamp(1.2rem,3vw,5rem)] text-white' />
+      <FaNotesMedical className='text-[clamp(1.7rem,3vw,5rem)] text-white' />
     </div>
   );
 };
